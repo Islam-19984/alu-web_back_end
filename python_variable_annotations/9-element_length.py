cc0746e4068cefa7e07e38fae2fb8af53eec988b
Islam-19984 <i.hemada@alustudent.com>
2024-09-06 23:42:33 +0000

    9-element_length.py

--- python_variable_annotations/9-element_length.py
+++ python_variable_annotations/9-element_length.py
@@ -1,12 +1,12 @@
 #!/usr/bin/env python3
-from typing import List, Tuple
+from typing import Iterable, Sequence, List, Tuple
 
-def element_length(lst: List[str]) -> List[Tuple[str, int]]:
+def element_length(lst: Iterable[Sequence]) -> List[Tuple[Sequence, int]]:
     """
-    Takes a list of strings and returns a list of tuples,
-    where each tuple contains a string and its length.
-
-    :param lst: List of strings
-    :return: List of tuples, each containing a string and its length
+    Takes an iterable of sequences and returns a list of tuples,
+    where each tuple contains a sequence and its length.
+    
+    :param lst: Iterable of sequences
+    :return: List of tuples, each containing a sequence and its length
     """
     return [(i, len(i)) for i in lst]
