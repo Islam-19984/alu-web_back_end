86bb41f2528ea2cc4d25632b66e304988c6de737
Islam-19984 <i.hemada@alustudent.com>
2024-09-06 20:47:48 +0000

    2-floor.py

--- python_variable_annotations/2-floor.py
+++ python_variable_annotations/2-floor.py
@@ -7,6 +7,6 @@ This module contains a function to compute the floor of a float.
 import math
 
 
-def floor(n: float)-> int:
+def floor(n: float) -> int:
     """Returns the floor of a float."""
     return math.floor(n)
