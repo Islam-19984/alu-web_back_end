187ae770c979aff34fca737b3828ea9541eff0f0
Islam-19984 <i.hemada@alustudent.com>
2024-09-06 20:20:19 +0000

    2-floor.py

---
+++ python_variable_annotations/2-floor.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+
+import math
+
+def floor(n: float)-> int:
+    """
+    
+    Returns the floor of a float.
+
+    Args:
+    n (float): The input float number.
+
+    Returns:
+        int: The floor of the float as an integer.
+        """
+
+        return math.floor(n)
