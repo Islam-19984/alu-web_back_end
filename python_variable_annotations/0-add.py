d8c48c876e5594afa7b32d7566ae1162cfb89ce3
Islam-19984 <i.hemada@alustudent.com>
2024-09-07 14:31:53 +0000

    0-add.py

--- python_variable_annotations/0-add.py
+++ python_variable_annotations/0-add.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python3
 
 def add(a: float, b: float) -> float:
-    """Return the sum of a and b as float."""
-    retrn a + b
+    """Returns the sum of a and b as a float."""
+    return a + b
